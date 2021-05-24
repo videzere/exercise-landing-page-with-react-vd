fc8ba8b93c5ea8dc6282380e9147ca188ede8adf
Victoria Dezerega <videzere@gmail.com>
2021-05-23 20:40:30 -0400

    Actualizar GitHud v3 from VS

--- src/js/component/FooterCards.js
+++ src/js/component/FooterCards.js
@@ -3,14 +3,13 @@ import { Navbar } from "react-bootstrap";
 
 export function FooterCards() {
 	return (
-		// <div className="bg-primary text-center m-t=100 p-12">
-		// 	<span>Copright &copy; Your Website 2019</span>
-		// </div>
-		<Navbar expand="lg" bg="primary" sticky="bottom">
-			<div>
-				<span className="text-center p-0 m-0">
-					Copright &copy; Your Website 2019
-				</span>
+		<Navbar
+			expand="lg"
+			bg="dark"
+			variant="dark"
+			className="d-flex flex-wrap justify-content-center aling-items-center p-0 mt-5">
+			<div className="text-white p-4 mt-5">
+				Copright &copy; Your Website 2019
 			</div>
 		</Navbar>
 	);
--- src/js/component/Menu.js
+++ src/js/component/Menu.js
@@ -3,13 +3,26 @@ import { Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
 
 export function Menu() {
 	return (
-		<Navbar expand="lg" bg="dark" variant="dark">
-			<Navbar.Brand href="#home">Start Bootstrap</Navbar.Brand>
-			<Nav className="mr-auto">
-				<Nav.Link href="#home">Home</Nav.Link>
-				<Nav.Link href="#about">About</Nav.Link>
-				<Nav.Link href="#services">Services</Nav.Link>
-				<Nav.Link href="#contact">Contact</Nav.Link>
+		<Navbar
+			expand="lg"
+			bg="dark"
+			variant="dark"
+			className="d-flex flex-wrap justify-content-around mx-2">
+			<Nav>
+				<Navbar.Brand href="#home">Start Bootstrap</Navbar.Brand>
+			</Nav>
+			<Nav>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link className="text-white" href="#home">
+							Home
+						</Nav.Link>
+						<Nav.Link href="#about">About</Nav.Link>
+						<Nav.Link href="#services">Services</Nav.Link>
+						<Nav.Link href="#contact">Contact</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
 			</Nav>
 		</Navbar>
 	);
